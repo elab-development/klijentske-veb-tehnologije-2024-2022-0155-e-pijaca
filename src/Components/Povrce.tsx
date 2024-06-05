7c387096de451ed9c3da5d75c37d141ffd933ae3
AntoninaProtic <antoninaprrotic@gmail.com>
2024-06-05 16:06:22 +0200

    Implementirana paginacija na stranici Povrce

--- src/Components/Povrce.tsx
+++ src/Components/Povrce.tsx
@@ -1,7 +1,7 @@
 import { Product } from '../models/Product'
 import IndividualP from './IndividualP';
 import '../css/VocePovrce.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 interface povrceProps{
     povrce: Product[];
     onAdd: (id: number) => void;
@@ -9,25 +9,38 @@ interface povrceProps{
     resetCategory: () => void;
 }
 
+
 const Povrce: React.FC<povrceProps> = ({povrce , onAdd, onDelete, resetCategory}) => {
-  
+  const [visibleProducts, setVisibleProducts] = useState(2);
+
+
   useEffect(() => {
     resetCategory();
   }, []);
-  
+
+
+  const showMoreProducts = () => {
+    setVisibleProducts(prevVisibleProducts => prevVisibleProducts + 2);
+  };
+ 
   return (
-    
    
+    <div className="povrce-ind-container">
         <div className="povrce-ind">
-        {povrce.length ===0 ? "Nema proizvoda" : povrce.map((p) => (
-        <IndividualP key={p.id} 
-        oneP={p} 
+        {povrce.length ===0 ? "Nema proizvoda" : povrce.slice(0, visibleProducts).map((p) => (
+        <IndividualP key={p.id}
+        oneP={p}
         onAdd={() => onAdd(p.id)}
         onDelete={() => onDelete(p.id)}/>
         ))}
-        </div>
-  
-  )
+        </div>{visibleProducts < povrce.length && (
+      <button className="show-more-btn" onClick={showMoreProducts}>Prikaži više</button>
+    )}
+  </div>
+);
+ 
+ 
 }
+
 
 export default Povrce
