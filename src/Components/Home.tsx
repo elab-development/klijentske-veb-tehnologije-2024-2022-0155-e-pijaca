5161c5815b458aeef7f3a5ee7dceb0e626f5ac64
AntoninaProtic <antoninaprrotic@gmail.com>
2024-06-05 16:38:31 +0200

    Implementirana paginacija na Home stranici

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Deo1 from './Deo1';
 import Popular from './Popular';
 import Proizvod from '../models/Proizvod';
 import { Product } from '../models/Product';
 import IndividualP from './IndividualP';
 import '../css/Home.css';
+
+
 
 
 interface HomeProps{
@@ -14,9 +16,18 @@ interface HomeProps{
   onDelete: (id: number) => void;
 }
 
+
 const Home: React.FC <HomeProps>= ({proizvod , products, onAdd, onDelete}) => {
+  const [visibleProducts, setVisibleProducts] = useState(3);
+
+
+  const showMoreProducts = () => {
+    setVisibleProducts(prevVisibleProducts => prevVisibleProducts + 3);
+  };
+
+
   return (
-    <div>
+    <div >
       <Deo1 />
      
       <div className="home-container">
@@ -24,14 +35,18 @@ const Home: React.FC <HomeProps>= ({proizvod , products, onAdd, onDelete}) => {
         <h1>Katalog Svežeg Voća i Povrća</h1>
 <p>Dobrodošli u naš sveobuhvatni katalog svežeg voća i povrća. Ovde možete pronaći širok izbor najkvalitetnijih plodova prirode, pažljivo odabranih i dostupnih za vašu svakodnevnu ishranu. Pogledajte našu ponudu i odaberite zdrave namirnice za vas i vašu porodicu.</p>
         </div>
-        {products.map((p)=>
+        {products.slice(0, visibleProducts).map((p)=>
         <IndividualP oneP={p}
         onAdd={() => onAdd(p.id)}
         onDelete={() => onDelete(p.id)}/>)}
       </div>
-     
+      {visibleProducts < products.length && (
+      <button className="show-more-btn" onClick={showMoreProducts}>Prikaži više</button>
+    )}
         <Popular proizvod={proizvod} onAdd={onAdd} onDelete={onDelete}/>
        
+
+
 
 
     </div>
@@ -39,5 +54,6 @@ const Home: React.FC <HomeProps>= ({proizvod , products, onAdd, onDelete}) => {
 }
 
 
-export default Home;
 
+
+export default Home;
